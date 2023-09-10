b103e3e1de3c25f1117ffaac591a64b27fa1506e
Gabriel V. Costa <gbrielcsta@yahoo.com>
2023-09-10 19:53:53 -0300

    .

--- data.m
+++ data.m
@@ -5,7 +5,7 @@ clc
 
 % Parameter Definitions
 U = 14.607;               % Inlet Velocity [m/s]
-AoA = 5;                  % Angle of Attack [degrees]
+AoA = 0;                  % Angle of Attack [degrees]
 I = 0.01;                 % Turbulence Intensity
 C = 1.00;                 % Chord length [m]
 
@@ -16,10 +16,10 @@ Ux = cos(AoA * PI / 180) * U;  % X-Velocity Component
 Uy = sin(AoA * PI / 180) * U;  % Y-Velocity Component
 
 % Calculation of Other Variables
-Lx = -sin(AoA * PI / 180);
-Ly = cos(AoA * PI / 180);
-Dx = cos(AoA * PI / 180);
-Dy = sin(AoA * PI / 180);
+Lx = -sin(AoA * pi / 180);
+Ly = cos(AoA * pi / 180);
+Dx = cos(AoA * pi / 180);
+Dy = sin(AoA * pi / 180);
 
 % Calculation of Turbulence-related Variables
 k = (3/2) * (U * I)^2;
